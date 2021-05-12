4f8d84f3ebf6184405bf8b86824482df8ee1c98c
sockelganger <83567628+sockelganger@users.noreply.github.com>
2021-05-11 22:06:40 -0700

    Update publish.c

--- publish.c
+++ publish.c
@@ -1,26 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printArr(int a[]);
+#define ITERATIONS 10 //how many numbers we're taking
 
-int main()
-{
-    int n[4] = {0,1,2,3};
-    printf("size of the array = %d\n",sizeof(n));
-    printf("Size of an element = %d\n",sizeof(n[0]));
-    printf("size of the element / array = %d\n",sizeof(n)/sizeof(n[0]));
+float loop_solution (int a);
+float recursive (int a, float b);
+float workNum;
+float sum;
 
-    printArr(n);
+int main(){
+    char choice;
+    float result;
+    printf("Please choose if you would like to use\n");
+    printf("[1] a loop solution\n");
+    printf("[2] a recursive solution\n");
+    printf("\t>");
+    scanf(" %c",&choice);
+
+    if (choice == '1'){
+        result = loop_solution(ITERATIONS);
+    }else if (choice == '2'){
+        result = recursive(ITERATIONS, 0);
+    }else{
+        printf("You have to choose [1] or [2]\n");
+        main();
+    }
+    printf("The Median of the numbers you entered is = %.2f",(result/ITERATIONS));
 
 return 0;
 }
 
-void printArr(int arr[]){
-    printf("size of the array = %d\n",sizeof(arr));
-    printf("Size of an element = %d\n",sizeof(arr[0]));
-    printf("size of the element / array = %d\n",sizeof(arr)/sizeof(arr[0]));
-
-    for (int i = 0; i <= (int)(sizeof(arr)/sizeof(arr[0])); i++){
-        printf("%d val = %d\n", i, arr[i]);
+float loop_solution (int count){
+    for(int i = 1; i <= count; i++){
+            printf("\nenter number %d>",i);
+            scanf("%f",&workNum);
+            sum+=workNum;
     }
+    return sum;
+}
+
+float recursive (int count, float f){
+            if(count == 0){
+                return f;
+            }
+            printf("\nenter number %d>",count);
+            scanf("%f",&workNum);
+            f+=workNum;
+            recursive(count -1, f);
+            //return f; // if entered here this will dismiss the error message but cause the return value to be incorrect.
 }
