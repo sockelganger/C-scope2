9a80ac557d8dbf45203dbf4a4408410ce4ead43b
sockelganger <83567628+sockelganger@users.noreply.github.com>
2021-05-14 20:24:43 -0700

    Update publish2.c

--- publish2.c
+++ publish2.c
@@ -3,22 +3,31 @@
 #include <time.h>
 #define MaxNodeNum 200
 #define MinNodeNum 100
-#define MaxPathsPerNode 199;
+#define MaxPathsPerNode 199
+#define MinPathsPerNode 1
+#define TreasureChancePercent 50
 
 int randGen(int a , int b);
+void buryTreasures(char a[][][],int size);
 
 int nodeNum;
 
 
 int main(){
-    nodeNum = randGen(MaxNodeNum, MinNodeNum);
+    nodeNum = randGen(MaxNodeNum, MinNodeNum);//generate number of nodes
+    char pathMap[nodeNum][(nodeNum*4)+1][2]; //[nodeNumber][PathSring(Node Names have 4 char)][T/F treasure]
+    int size = 3;
+    buryTreasures(pathMap[][][],size); //bury the treasures
+
+    //define absolute paths to node
+
+    //write absolute path to array
+
+    //define treasure condition at node
 
 
     return(0);
    }
-
-
-
 
 
 
@@ -29,5 +38,9 @@ int randGen(int max, int min){
    value = (rand() % (max-min+1)+min);
    printf("%d\n", value);
    return value;
+}
+
+void buryTreasures(char a[][][],int size){
+
 
 }
